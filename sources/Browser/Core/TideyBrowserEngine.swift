f0564db935ffc9722ec8ce17f258c6cd4e487493
Tim-Feng <fsjforever26@gmail.com>
2026-04-20 18:23:32 +0800

    [STRUCTURAL] Add shared browser engine state callbacks

--- sources/Browser/Core/TideyBrowserEngine.swift
+++ sources/Browser/Core/TideyBrowserEngine.swift
@@ -8,8 +8,13 @@ import WebKit
 
 @MainActor
 final class TideyBrowserEngine: NSObject {
-    weak var host: TideyBrowserEngineHost?
+    weak var host: TideyBrowserEngineHost? {
+        didSet {
+            notifyStateDidChange()
+        }
+    }
     let webView: WKWebView
+    private var observations: [NSKeyValueObservation] = []
 
     convenience init(host: TideyBrowserEngineHost? = nil) {
         self.init(configuration: Self.defaultConfiguration(), host: host)
@@ -22,6 +27,7 @@ final class TideyBrowserEngine: NSObject {
         super.init()
         webView.navigationDelegate = self
         webView.uiDelegate = self
+        configureStateObservations()
     }
 
     static func defaultConfiguration() -> WKWebViewConfiguration {
@@ -50,6 +56,14 @@ final class TideyBrowserEngine: NSObject {
         webView.canGoForward
     }
 
+    var state: TideyBrowserEngineState {
+        TideyBrowserEngineState(currentURL: url,
+                                currentTitle: title,
+                                isLoading: isLoading,
+                                canGoBack: canGoBack,
+                                canGoForward: canGoForward)
+    }
+
     func load(_ url: URL) {
         webView.load(URLRequest(url: url))
     }
@@ -64,6 +78,30 @@ final class TideyBrowserEngine: NSObject {
 
     func reload() {
         webView.reload()
+    }
+
+    private func configureStateObservations() {
+        observations = [
+            webView.observe(\.url, options: [.initial, .new]) { [weak self] _, _ in
+                self?.notifyStateDidChange()
+            },
+            webView.observe(\.title, options: [.initial, .new]) { [weak self] _, _ in
+                self?.notifyStateDidChange()
+            },
+            webView.observe(\.isLoading, options: [.initial, .new]) { [weak self] _, _ in
+                self?.notifyStateDidChange()
+            },
+            webView.observe(\.canGoBack, options: [.initial, .new]) { [weak self] _, _ in
+                self?.notifyStateDidChange()
+            },
+            webView.observe(\.canGoForward, options: [.initial, .new]) { [weak self] _, _ in
+                self?.notifyStateDidChange()
+            }
+        ]
+    }
+
+    private func notifyStateDidChange() {
+        host?.browserEngine(self, didUpdateState: state)
     }
 }
 
--- sources/Browser/Core/TideyBrowserEngineHost.swift
+++ sources/Browser/Core/TideyBrowserEngineHost.swift
@@ -7,7 +7,18 @@ import Foundation
 import WebKit
 
 @MainActor
+struct TideyBrowserEngineState: Equatable {
+    let currentURL: URL?
+    let currentTitle: String?
+    let isLoading: Bool
+    let canGoBack: Bool
+    let canGoForward: Bool
+}
+
+@MainActor
 protocol TideyBrowserEngineHost: AnyObject {
+    func browserEngine(_ engine: TideyBrowserEngine,
+                       didUpdateState state: TideyBrowserEngineState)
     func browserEngine(_ engine: TideyBrowserEngine,
                        requestOpenNewTabFor url: URL,
                        configuration: WKWebViewConfiguration)
